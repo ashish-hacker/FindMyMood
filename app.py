d90bd51bd4bc1012f434e5155b2a4a9432df0350
Ashish <54330052+ashish-hacker@users.noreply.github.com>
2020-06-26 11:57:03 +0530

    added the app skeleton

---
+++ app.py
@@ -0,0 +1,22 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/cake')
+def cake():
+    return render_template('cake.html') 
+
+
+@app.route('/hello/<name>') 
+def hello(name):
+    return render_template('page.html', name=name)      
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
